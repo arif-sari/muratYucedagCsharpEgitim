13316fc2d96d93a8da71480b1fb168c41c3d7e4a
Arif SARI <72504353+arif-sari@users.noreply.github.com>
2024-10-09 23:03:29 +0300

    Update 03_MakingDecision.cs

--- 03_MakingDecision.cs
+++ 03_MakingDecision.cs
@@ -115,7 +115,145 @@ namespace _03_Making_Decision
             //{
             //    Console.WriteLine("Hoşgeldiniz "+ username );
             //}
-                        #endregion
+            #endregion
+
+            #region Mod işlemleri
+
+            //int number;
+            //number = 26;
+
+            //int result =number%5;
+
+            //Console.WriteLine(result);
+
+            //Console.WriteLine("Lütfen 1. sayıyı giriniz:");
+            //int number1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Lütfen 2. sayıyı giriniz:");
+            //int number2 = int.Parse(Console.ReadLine());
+
+            //int result= number1%number2;
+
+            //Console.WriteLine("Birinci sayının ikinci sayıya bölümünden kalan: " + result);
+
+
+            //Console.Read();
+
+
+            //Console.WriteLine("Lütfen sayıyı giriniz: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("Sayı çift sayıdır");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayı tek sayidir.");
+            //}
+            #endregion
+
+            #region Char Değişkenler ilt Karar Yapıları
+
+
+            //char team;
+            //Console.Write("Lütfen takım rengi girin: ");
+            //team = char.Parse(Console.ReadLine());
+
+            //if (team == 'g' | team == 'G')
+            //{
+            //    Console.WriteLine("Galatasayar");
+            //}
+            //if (team == 'f' | team == 'F')
+            //{
+            //    Console.WriteLine("Fenerbahçe");
+            //}
+            //if (team == 'b' | team == 'B')
+            //{
+            //    Console.WriteLine("Beşiktaş");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Takım tanımsız");
+            //}
+
+
+
+
+            #endregion
+
+            #region
+            Console.WriteLine("***** Csharp Eğitim Kamp Restaurant *****");
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("1-Anayemekler");
+            Console.WriteLine("2-Çorbalar");
+            Console.WriteLine("3_Pizzalar");
+            Console.WriteLine("4-İçeceker");
+            Console.WriteLine("5-Tatlılar");
+            Console.WriteLine("----------------------------------");
+
+            string menuItem;
+            Console.Write("Lütfen detayını görmek istediğiniz menu seçin:");
+            menuItem= Console.ReadLine();
+            Console.WriteLine();
+
+            if (menuItem == "1")
+            {
+                Console.WriteLine("----------------Ana yemekler------------------");
+                Console.WriteLine();
+                Console.WriteLine("1-Köri soslu tavuk");
+                Console.WriteLine("2-Kızartma Tabağı");
+                Console.WriteLine("3-Fasulye pilav" );
+                Console.WriteLine("4-Fırında Somon");
+                Console.WriteLine("5-Patlıcan Musakka");
+            }
+
+            if (menuItem == "2")
+            {
+                Console.WriteLine("----------------Çorbalar------------------");
+                Console.WriteLine();
+                Console.WriteLine("1-Mercimek");
+                Console.WriteLine("2-Ezogelin");
+                Console.WriteLine("3-Tavuk");
+              
+            }
+
+            if (menuItem == "3")
+            {
+                Console.WriteLine("----------------Pizzalar------------------");
+                Console.WriteLine();
+                Console.WriteLine("1-Akdeniz");
+                Console.WriteLine("2-Margarita");
+                Console.WriteLine("3-Tonbalıklı");
+               
+            }
+
+            if (menuItem == "4")
+            {
+                Console.WriteLine("----------------İçecekler------------------");
+                Console.WriteLine();
+                Console.WriteLine("1-Kola");
+                Console.WriteLine("2-Ayran");
+                Console.WriteLine("3-Su");
+                
+            }
+
+            if (menuItem == "5")
+            {
+                Console.WriteLine("----------------Tatlıllar------------------");
+                Console.WriteLine();
+                Console.WriteLine("1-Sütlç");
+                Console.WriteLine("2-Pasta");
+                Console.WriteLine("3-Profiterol");
+                Console.WriteLine("4-Dondurma");
+              
+            }
+
+
+
+            #endregion
+
 
             Console.Read();
 
