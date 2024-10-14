c720983ee6411e1fe2bc80b8d084b9fab6f5e16d
Arif SARI <72504353+arif-sari@users.noreply.github.com>
2024-10-15 00:29:37 +0300

    Update 05_LoopWithStars.cs

--- 05_LoopWithStars.cs
+++ 05_LoopWithStars.cs
@@ -11,45 +11,124 @@ namespace _05_LoopWithStars
         static void Main(string[] args)
         {
             #region Alt alta on adet yıldız
-            //for (int i =1;i<=10;i++)
+
+            //for (int i = 1; i <= 10; i++)
             //{
             //    Console.WriteLine("*");
-            //}
 
+            //}
 
             #endregion
 
             #region Yan yana 10 adet yıldız
-            //for (int i=1;i<=10;i++)
+
+            //for (int i = 1; i <= 10; i++)
             //{
             //    Console.Write("*");
             //}
 
+
+
             #endregion
 
             #region Alt alta alta 10 tane yıldız oluşturma her satırda 10 adet yıldız
-
             //for(int i = 1; i <= 10; i++)
-            //{
-            //    Console.WriteLine("**********");
-            //}
+            // {
+            //     Console.WriteLine("**********");
+            // }
+
 
             #endregion
 
             #region Dik üçgen yıldız
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
 
-            for (int i = 1; i <= 5; i++)
-            {
-                
-                for (int j = 1; j <= 2; j++)
-                {
-                    Console.Write("*");                  
-                }
-                Console.WriteLine();
-            }
+            #region ters Dikücgen
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
 
             #endregion
+            #region dik ve ters dik üçgen beraber
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for(int k = 4; k >= 1; k--)
+            //{
+            //    for(int m = 1; m <= k; m++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region baklava dilimi
+
+            //üst kısım 
+
+            //int n = 5;
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for(int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for(int k=1;k<=2*i-1;k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            ////alt kısım
+
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = n - 1;j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for(int k=1; k<=2*i-1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }   
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region
+
+
+            #endregion
+
             Console.Read();
-        }
+        } 
     }
 }
+
+
